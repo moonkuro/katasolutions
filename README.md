ebac1f831427fbe96901d49643bbef2a04217e2b
ograndefio <mnevesgloria1@gmail.com>
2024-05-01 19:36:04 -0300

    Created folders for other studies

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Kata Solutions
-[codewars.com/moonkuro](https://www.codewars.com/users/moonkuro)
-Just trying to do the codewars puzzles and posting the solutions here. 
+[codewars.com/moonkuro](https://www.codewars.com/users/moonkuro) Just python 4fun.
+
 
--- fundamentals/findneedle.py
+++ fundamentals/findneedle.py
@@ -1,7 +1,7 @@
 def find_needle(haystack):
-    for i, n in enumerate(haystack):
-        if n == "needle" or "NEEDLE" or "Needle":
-            return f"found the needle in {n}"
+    for position, name in enumerate(haystack):
+        if name == "needle":
+            return f"found the needle at position {position}"
 
 haystack = ['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]
 print(find_needle(haystack))
